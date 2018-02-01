1277987b33adc9b925bc8b21a79f6b2dbcb4bc4b
James Noss <jnoss@stsci.edu>
2018-02-01 15:05:05 -0500

    Performance enhancements for convolution.convolve
    - [Convolution investigation and benchmark](https://github.com/jamienoss/astropy/issues/1)
    - [Performance Notes](https://github.com/jamienoss/astropy/issues/2)
    
     ## Perf. changes
    
    - Separate code for interpolating NaN values and when not. __Major perf. boost.__
    - If ``nan_treatment=‘interpolate’``, pre-check image for presence of NaN values,
      if none exist, don’t call NaN interpolation code. __Major perf. boost.__
    - Hoist loop dependent variable computations to correct loop scope.
    - Untangle NaN interpolation and kernel normalization. Don’t normalize kernel until
      after convolution instead of doing so before and again after if not requested.
    - Thread with OPENMP. Required replacing Cython code (.pyx) with raw C. __Major perf. boost.__
    - For ``boundary in [‘fill’, ‘extend’, ‘wrap’]`` use a padded array instead of specific index logic. __Major perf. boost.__
      This has a significant performance boost, though it does increase the memory footprint.
      It also signifcantly reduces the code base.
    - Improve DASK performance as previous Cython code used Python division and thus
      meant reacquiring the GIL per loop iteration.
      _Note:_ this could have been solved by adding the decorator: ``@cython.cdivision(True)`` to the Cython functions.
      _Note:_ Using ``convolve`` threaded with DASK is still more performant than before however,
      this will overcommit threads and could temporarily make the machine unresponsive. When using DASK
      it is advised to make ``n_threads=1``, and can still improve runtime perf. by 2 compared to previous implementation.
      [github issue](https://github.com/jamienoss/astropy/issues/5)
    - Hoist all exceptions to as early as possible, especially before memory allocation or heavy(ish) computations.
    
     ## Non Perf. changes
    
    - Correct doc string for default boundary case ``None`` -> ``’fill’``.
    - Both the existing and previous (Cython/C) implementations of boundary=None require the kernel to be smaller
      than the image. This was never actually checked, it now is and an exception is raised.
    
    Signed-off-by: James Noss <jnoss@stsci.edu>

---
+++ astropy/convolution/setup_package.py
@@ -0,0 +1,24 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+import os
+from numpy import get_include as get_numpy_include
+from distutils.extension import Extension
+from astropy_helpers.openmp_helpers import add_openmp_flags_if_available
+
+C_CONVOLVE_PKGDIR = os.path.relpath(os.path.dirname(__file__))
+
+SRC_FILES = [os.path.join(C_CONVOLVE_PKGDIR, filename)
+              for filename in ['boundary_none_direct.c',
+                               'boundary_padded_direct.c']]
+
+def get_extensions():
+    # Add '-Rpass-missed=.*' to ``extra_compile_args`` when compiling with clang
+    # to report missed optimizations
+    c_convolve_ext = Extension(name='c_convolve', sources=SRC_FILES,
+                 extra_compile_args=['-O3', '-fPIC'],
+                 include_dirs=[get_numpy_include()],
+                 language='c')
+
+    add_openmp_flags_if_available(c_convolve_ext)
+    
+    return [c_convolve_ext]
