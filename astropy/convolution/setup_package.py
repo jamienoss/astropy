05d8e06f033415641f0f84a89f068199ea5967f8
James Noss <jnoss@stsci.edu>
2018-07-25 15:42:25 -0400

    Add -DMS_WIN64 to MS build
    Signed-off-by: James Noss <jnoss@stsci.edu>

--- astropy/convolution/setup_package.py
+++ astropy/convolution/setup_package.py
@@ -1,6 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 import os
+import sys
 from distutils.extension import Extension
 from astropy_helpers.openmp_helpers import add_openmp_flags_if_available
 
@@ -10,11 +11,18 @@ SRC_FILES = [os.path.join(C_CONVOLVE_PKGDIR, filename)
               for filename in ['src/boundary_none.c',
                                'src/boundary_padded.c']]
 
+extra_compile_args=['-UNDEBUG']
+# http://grokbase.com/t/python/cython-devel/148e9sy24z/cython-error-lnk2001-unresolved-external-symbol-pyinit-init
+if sys.platform.startswith('win'):
+     extra_compile_args.append('-DMS_WIN64')
+else:
+    extra_compile_args.append('-fPIC')
+
 def get_extensions():
     # Add '-Rpass-missed=.*' to ``extra_compile_args`` when compiling with clang
     # to report missed optimizations
     lib_convolve_ext = Extension(name='astropy.convolution.lib_convolve', sources=SRC_FILES,
-                 extra_compile_args=['-UNDEBUG', '-fPIC'],
+                 extra_compile_args=extra_compile_args,
                  include_dirs=["numpy"],
                  language='c')
 
